087c58438fb90f08502152e1cf8faa00ffb1ee33
Ikhlass <uiop@macbook-pro1.home>
2022-11-20 16:05:00 +0100

    debug7

--- routes/offer.js
+++ routes/offer.js
@@ -21,7 +21,7 @@ router.post(
   fileUpload(),
   async (req, res) => {
     try {
-      // const pictureToUpload = convertToBase64(req.files.picture);
+      const pictureToUpload = convertToBase64(req.files.picture);
 
       const newOffer = new Offer({
         product_name: req.body.product_name,
@@ -47,15 +47,14 @@ router.post(
 
         owner: req.user,
       });
-      //   const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
-      //     folder: `/vinted/offers/${newOffer._id}`,
-      // });
+      const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
+        folder: `/vinted/offers/${newOffer._id}`,
+      });
 
-      // newOffer.product_image = uploadResult;
+      newOffer.product_image = uploadResult;
 
       await newOffer.save();
       res.status(200).json({ newOffer });
-      res.status(200).json(req.user);
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
