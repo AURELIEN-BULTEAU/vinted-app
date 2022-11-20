6ceb2d02ac756e4b7fab3b858f06ebe166570814
Ikhlass <uiop@macbook-pro1.home>
2022-11-20 15:06:18 +0100

    debug1

--- routes/offer.js
+++ routes/offer.js
@@ -21,39 +21,39 @@ router.post(
   fileUpload(),
   async (req, res) => {
     try {
-      const pictureToUpload = convertToBase64(req.files.picture);
+      //   const pictureToUpload = convertToBase64(req.files.picture);
 
-      const newOffer = new Offer({
-        product_name: req.body.product_name,
-        product_description: req.body.product_description,
-        product_price: req.body.product_price,
-        product_details: [
-          {
-            MARQUE: req.body.brand,
-          },
-          {
-            TAILLE: req.body.size,
-          },
-          {
-            ÉTAT: req.body.condition,
-          },
-          {
-            COULEUR: req.body.color,
-          },
-          {
-            EMPLACEMENT: req.body.city,
-          },
-        ],
+      //   const newOffer = new Offer({
+      //     product_name: req.body.product_name,
+      //     product_description: req.body.product_description,
+      //     product_price: req.body.product_price,
+      //     product_details: [
+      //       {
+      //         MARQUE: req.body.brand,
+      //       },
+      //       {
+      //         TAILLE: req.body.size,
+      //       },
+      //       {
+      //         ÉTAT: req.body.condition,
+      //       },
+      //       {
+      //         COULEUR: req.body.color,
+      //       },
+      //       {
+      //         EMPLACEMENT: req.body.city,
+      //       },
+      //     ],
 
-        owner: req.user,
-      });
-      const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
-        folder: `/vinted/offers/${newOffer._id}`,
-      });
+      //     owner: req.user,
+      //   });
+      //   const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
+      //     folder: `/vinted/offers/${newOffer._id}`,
+      //   });
 
-      newOffer.product_image = uploadResult;
+      //   newOffer.product_image = uploadResult;
 
-      await newOffer.save();
+      //   await newOffer.save();
       res.status(200).json({ newOffer });
     } catch (error) {
       res.status(400).json({ message: error.message });
