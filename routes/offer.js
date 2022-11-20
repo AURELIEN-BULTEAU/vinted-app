c3d65aa29dde4914718aed0e7144c6ad5340de2a
Ikhlass <uiop@macbook-pro1.home>
2022-11-20 15:55:30 +0100

    debug6

--- routes/offer.js
+++ routes/offer.js
@@ -25,31 +25,31 @@ router.post(
 
       const newOffer = new Offer({
         product_name: req.body.product_name,
-        //   product_description: req.body.product_description,
-        //   product_price: req.body.product_price,
-        //   product_details: [
-        //     {
-        //       MARQUE: req.body.brand,
-        //     },
-        //     {
-        //       TAILLE: req.body.size,
-        //     },
-        //     {
-        //       ÉTAT: req.body.condition,
-        //     },
-        //     {
-        //       COULEUR: req.body.color,
-        //     },
-        //     {
-        //       EMPLACEMENT: req.body.city,
-        //     },
-        //   ],
+        product_description: req.body.product_description,
+        product_price: req.body.product_price,
+        product_details: [
+          {
+            MARQUE: req.body.brand,
+          },
+          {
+            TAILLE: req.body.size,
+          },
+          {
+            ÉTAT: req.body.condition,
+          },
+          {
+            COULEUR: req.body.color,
+          },
+          {
+            EMPLACEMENT: req.body.city,
+          },
+        ],
 
-        //   owner: req.user,
-        // });
-        // const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
-        //   folder: `/vinted/offers/${newOffer._id}`,
+        owner: req.user,
       });
+      //   const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
+      //     folder: `/vinted/offers/${newOffer._id}`,
+      // });
 
       // newOffer.product_image = uploadResult;
 
