0c383dcf2851ae20f313d31adf72b5ad3b48bcd9
Ikhlass <uiop@macbook-pro1.home>
2022-11-20 15:21:32 +0100

    debug3

--- routes/offer.js
+++ routes/offer.js
@@ -1,27 +1,10 @@
-const express = require("express");
-const router = express.Router();
-const isAuthenticated = require("../middlewares/isAuthenticated");
-const fileUpload = require("express-fileupload");
-
-const Offer = require("../models/Offer");
-
-const cloudinary = require("cloudinary").v2;
-const convertToBase64 = (file) => {
-  return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
-};
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
 router.post(
   "/offer/publish",
   isAuthenticated,
   fileUpload(),
   async (req, res) => {
     try {
-      //   const pictureToUpload = convertToBase64(req.files.picture);
+      /*const pictureToUpload = convertToBase64(req.files.picture);
 
       const newOffer = new Offer({
         product_name: req.body.product_name,
@@ -47,17 +30,17 @@ router.post(
 
         owner: req.user,
       });
-      //   const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
-      //     folder: `/vinted/offers/${newOffer._id}`,
-      //   });
+      const uploadResult = await cloudinary.uploader.upload(pictureToUpload, {
+        folder: `/vinted/offers/${newOffer._id}`,
+      });
 
-      //   newOffer.product_image = uploadResult;
+      newOffer.product_image = uploadResult;
 
-      //   await newOffer.save();
-      res.status(200).json({ newOffer });
+      await newOffer.save();
+      res.status(200).json({ newOffer });*/
+      res.status(200).json(req.user);
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
   }
 );
-module.exports = router;
