08937f40e523db5bdd1f5c7759667b1cd711d775
Ikhlass <uiop@macbook-pro1.home>
2023-02-10 20:07:02 +0100

    comment .select

--- routes/list-offer.js
+++ routes/list-offer.js
@@ -34,10 +34,10 @@ router.get("/offer", async (req, res) => {
     const listOffer = await Offer.find(filters)
       .sort(sortValue)
       .limit(limit)
-      .skip((page - 1) * limit)
-      .select(
-        "product_name product_price product_image product_description product_details _id"
-      );
+      .skip((page - 1) * limit);
+    // .select(
+    //   "product_name product_price product_image product_description product_details _id"
+    // );
 
     const count = await Offer.find(filters).countDocuments();
     res.status(200).json({ count: count, offers: listOffer });
